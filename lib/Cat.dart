a23d6d45dbb0702d729e90b7c69a143ce5cf4082
bernadette1008 <vachiv@naver.com>
2025-03-27 16:23:45 +0900

    고양이에 대한 상세한 내용 추가

--- lib/Cat.dart
+++ lib/Cat.dart
@@ -4,6 +4,20 @@ import 'package:flame/events.dart';
 class MyCat extends SpriteComponent with TapCallbacks, DragCallbacks, HasGameRef{
   String img = "Player.gif";
 
+  // 고양이의 나이 - 시간이 흐르면 증가, 일정 값을 넘으면 엔딩 이때 happy 값에 따라 다른 엔딩 출력
+  int age = 0;
+  
+  // 시간마다 두 값은 하락하지만 플레이어의 케어로 증가시킬 수 있는 값
+  var hungry = 100;
+  var sleep = 100;
+
+  // 시간마다 hungry와 sleep 값을 측정하여 지정된 값보다 낮으면 happy 값이 하락 높으면 happy 값 증가
+  // happy 값이 너무 낮으면 바로 엔딩 출력하도록 하기
+  var happy = 0; 
+
+  bool isMove = false;
+  bool isTouch = false;
+
   MyCat({super.position}):
         super(
         //고양이 크기
@@ -11,6 +25,17 @@ class MyCat extends SpriteComponent with TapCallbacks, DragCallbacks, HasGameRef
         //이미지 앵커
         anchor: Anchor.center,
       );
+
+  void FallASleep(){
+    while(isMove == false && isTouch == false){
+      // TODO: 지정된 시간마다 sleep 값이 증가하도록 코드 추가
+      sleep += 10;
+      if(sleep > 100){
+        sleep = 100;
+        break;
+      }
+    }
+  }
 
   @override
   Future<void> onLoad() async{
@@ -22,11 +47,13 @@ class MyCat extends SpriteComponent with TapCallbacks, DragCallbacks, HasGameRef
   @override
   void onTapDown(TapDownEvent event) async{
     img = "TapPlayer.gif";
+    isTouch = true;
     sprite = await Sprite.load(img);
   }
   @override
   void onTapUp(TapUpEvent event) async{
     img = "Player.gif";
+    isTouch = false;
     sprite = await Sprite.load(img);
   }
 
@@ -35,6 +62,7 @@ class MyCat extends SpriteComponent with TapCallbacks, DragCallbacks, HasGameRef
   @override
   void onDragStart(DragStartEvent event) async{
     img = "MovePlayer.gif";
+    isMove = true;
     sprite = await Sprite.load(img);
   }
 
@@ -60,6 +88,8 @@ class MyCat extends SpriteComponent with TapCallbacks, DragCallbacks, HasGameRef
   @override
   void onDragEnd(DragEndEvent event) async{
     img = "Player.gif";
+    isMove = false;
+    isTouch = false;
     sprite = await Sprite.load(img);
   }
   
